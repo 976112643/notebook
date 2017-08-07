499a2aad05c064cd3dc4e953639d48373e7b161c
淡定的程序员 <976112643@qq.com>
2017-08-07 23:52:49 +0800

    Realm数据库实体类创建

--- app/src/main/java/com/wq/common/db/DBUtil.kt
+++ app/src/main/java/com/wq/common/db/DBUtil.kt
@@ -9,7 +9,6 @@ import io.realm.RealmObject
 val realm: Realm
     get() = Realm.getDefaultInstance()
 
-
 /**
  * 给所有对象扩展数据库事务处理函数
  */
@@ -21,28 +20,12 @@ fun executeTransaction(transaction: (realm: Realm) -> Unit) {
     }
 }
 
+inline fun <reified T : RealmObject> where() = realm.where(T::class.java)
+
+inline fun <reified T : RealmObject> findAll() = where<T>().findAll()
 
 /**
  * 给数据库对象字段扩展id生成方法
  */
-@Synchronized fun RealmObject.generateId():String=(realm.where(this.javaClass).count() + 1).toString()
+@Synchronized fun RealmObject.generateId(): String = (realm.where(this.javaClass).count() + 1).toString()
 
-
-
-//fun <T> Any.useDB(call: SQLiteDatabase.() -> T): T = MyDatabaseOpenHelper.getInstance(_CONTEXT).use(call)
-
-/**
- * Created by weiquan on 2017/8/1.
- */
-//inline fun <reified T : Any> Any.findAll(page: Int = 0, selectMap: Map<String, Any> = mapOf<String, Any>()): List<T> = useDB {
-//    select(T::class.java.simpleName).whereArgs(linkWhereArgs(selectMap)).limit(page * 10, 10).parseList(classParser<T>())
-//}
-
-fun linkWhereArgs(selectMap: Map<String, Any>): String {
-    val sBuilder = StringBuilder()
-    for ((key, value) in selectMap) {
-        sBuilder.append(key).append(" = ").append("'$value'").append(" AND ")
-    }
-    if (sBuilder.isNotEmpty()) sBuilder.delete(sBuilder.length - 5, sBuilder.length)
-    return sBuilder.toString()
-}
